65ebb6e5912b50de78309c566710415e8be6f84b
nvs <vansi18042001@gmail.com>
2024-05-25 13:55:37 +0700

    init project

---
+++ src/test/java/com/nvs/th_java/ThJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvs.th_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
