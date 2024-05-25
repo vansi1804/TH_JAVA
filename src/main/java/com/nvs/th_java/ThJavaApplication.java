021c73ec15744494d63ff44a488e07d75893611a
nvs <vansi18042001@gmail.com>
2024-05-25 15:46:30 +0700

    1.5

--- src/main/java/com/nvs/th_java/ThJavaApplication.java
+++ src/main/java/com/nvs/th_java/ThJavaApplication.java
@@ -2,8 +2,10 @@ package com.nvs.th_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ThJavaApplication {
 
 	public static void main(String[] args) {
