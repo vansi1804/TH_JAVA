652383445455bf4d4359df84406c164ee71e44a1
nvs <vansi18042001@gmail.com>
2024-05-25 14:13:25 +0700

    1.4.1

---
+++ src/main/java/com/nvs/th_java/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.nvs.th_java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public  String hello(Model model){
+        model.addAttribute("message","Hello bro");
+        return "home/home";
+    }
+}
